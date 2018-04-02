20dc680bb40d1182e4695a70909cb0d13a51a2d4
Callum Mackenzie <callum1mackenzie@gmail.com>
2018-04-02 09:30:11 +0100

    completed add_new_pet_to_stock

--- pet_shop_spec.rb
+++ pet_shop_spec.rb
@@ -107,10 +107,10 @@ class TestPetShop < Minitest::Test
   #   assert_equal(2, sold)
   # end
 
-  # def test_stock_count
-  #   count = stock_count(@pet_shop)
-  #   assert_equal(6, count)
-  # end
+  def test_stock_count
+    count = stock_count(@pet_shop)
+    assert_equal(6, count)
+  end
 
   # def test_all_pets_by_breed__found
   #   pets = pets_by_breed(@pet_shop, "British Shorthair")
@@ -140,16 +140,16 @@ class TestPetShop < Minitest::Test
   #   assert_nil(pet)
   # end
   #
-  # def test_add_pet_to_stock
-  #   add_pet_to_stock(@pet_shop, @new_pet)
-  #   count = stock_count(@pet_shop)
-  #   assert_equal(7, count)
-  # end
-
-  def test_customer_pet_count
-    count = customer_pet_count(@customers[0])
-    assert_equal(0, count)
+  def test_add_pet_to_stock
+    add_pet_to_stock(@pet_shop, @new_pet)
+    count = stock_count(@pet_shop)
+    assert_equal(7, count)
   end
+
+  # def test_customer_pet_count
+  #   count = customer_pet_count(@customers[0])
+  #   assert_equal(0, count)
+  # end
 
   # def test_add_pet_to_customer
   #   customer = @customers[0]
